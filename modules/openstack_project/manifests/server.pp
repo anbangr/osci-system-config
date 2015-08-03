2b2d20ea9dd68cd090e3bafab01cb1ed348ecb43
aruan <aruan@foreman.lab.100percentit.com>
2015-08-03 15:07:44 +0200

    remove puppetmaster.openstack.org dependency

--- modules/openstack_project/manifests/server.pp
+++ modules/openstack_project/manifests/server.pp
@@ -11,7 +11,7 @@ class openstack_project::server (
   $pin_puppet                = '3.',
   $ca_server                 = undef,
   $afs                       = false,
-  $puppetmaster_server       = 'puppetmaster.openstack.org',
+  $puppetmaster_server       = 'jenkins.lab.100percentit.com',
   $manage_exim               = true,
   $pypi_index_url            = 'https://pypi.python.org/simple',
   $pypi_trusted_hosts        = [
--- modules/openstack_project/manifests/template.pp
+++ modules/openstack_project/manifests/template.pp
@@ -15,7 +15,7 @@ class openstack_project::template (
   $ca_server                 = undef,
   $enable_unbound            = true,
   $afs                       = false,
-  $puppetmaster_server       = 'puppetmaster.openstack.org',
+  $puppetmaster_server       = 'jenkins.lab.100pecentit.com',
   $manage_exim               = false,
   $sysadmins                 = [],
   $pypi_index_url            = 'https://pypi.python.org/simple',
