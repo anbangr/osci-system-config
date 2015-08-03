518a01be9a479780ea581a40dbc0c690a65f2c14
aruan <aruan@foreman.lab.100percentit.com>
2015-08-03 17:24:52 +0200

    remove server in gerrit def

--- manifests/site.pp
+++ manifests/site.pp
@@ -15,11 +15,11 @@
 #}
 
 node 'jenkins.lab.100percentit.com' {
-  class { 'openstack_project::server':
-    iptables_public_tcp_ports => [4505, 4506, 8140],
-    sysadmins                 => hiera('sysadmins', []),
-    pin_puppet                => '3.6.',
-  }
+ # class { 'openstack_project::server':
+ #   iptables_public_tcp_ports => [4505, 4506, 8140],
+ #   sysadmins                 => hiera('sysadmins', []),
+ #   pin_puppet                => '3.6.',
+ # }
 
   class { 'openstack_project::puppetmaster':
     root_rsa_key => hiera('puppetmaster_root_rsa_key', 'XXX'),
